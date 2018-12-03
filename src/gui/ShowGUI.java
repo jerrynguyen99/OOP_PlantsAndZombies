a08b18595f69d0475d265af6a6fb3c79dc676fea
Jerry Nguyen <nguyentruongdat1661999@gmail.com>
2018-12-03 11:54:00 +0700

    Background is not setup yet ...

--- src/gui/ShowGUI.java
+++ src/gui/ShowGUI.java
@@ -9,7 +9,6 @@ import org.newdawn.slick.state.StateBasedGame;
 import java.io.File;
 
 public class ShowGUI extends StateBasedGame {
-
     //Windows Size
     protected static int width 	= 1600;
     protected static int height 	= 900;
@@ -63,5 +62,8 @@ public class ShowGUI extends StateBasedGame {
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
 
+    }
+    public static boolean isAA() {
+        return AA;
     }
 }
---
+++ src/jerry/Intro.java
@@ -0,0 +1,45 @@
+package jerry;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import static gui.ShowGUI.isAA;
+
+public class Intro extends BasicGameState {
+
+    private Image background;
+    public Image logo;
+    public Image playButton;
+
+    /**
+     * Create splash screen
+     * @param state	State index
+     */
+    public Intro(int state) {
+
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+        gameContainer.getGraphics().setAntiAlias(isAA());
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+
+    }
+}
